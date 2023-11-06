86d1691774b8085c57b9124de9d487806225b7c3
Denis <Kds04042002omgu@gmail.com>
2023-11-06 16:06:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/thirdtask/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.thirdtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout mConstraintLayout;
+    private TextView mInfoTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mConstraintLayout = findViewById(R.id.root_layout);
+        mInfoTextView = findViewById(R.id.textView);
+        Button yellowButton = (Button) findViewById(R.id.button_yellow);
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInfoTextView.setText(R.string.yellow);
+                mConstraintLayout.setBackgroundColor(ContextCompat
+                        .getColor(MainActivity.this, R.color.yellowColor));
+            }
+        });
+    }
+    public void onRedButtonClick(View view) {
+        mInfoTextView.setText(R.string.red);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.redColor));
+    }
+    public void onGreenButtonClick(View view) {
+        mInfoTextView.setText(R.string.Green);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.greenColor));
+    }
+}
---
+++ app/src/main/java/com/example/thirdtask/TextView.java
@@ -0,0 +1,4 @@
+package com.example.thirdtask;
+
+public class TextView {
+}
